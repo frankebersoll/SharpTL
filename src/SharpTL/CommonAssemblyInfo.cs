c78dc2cc8dbc587a904b97eae1c0e2a3ab74a477
Alexander Logger <intagger@live.ru>
2014-02-15 19:58:18 +0400

    Updated copyright and version to 0.6.

--- src/SharpTL/CommonAssemblyInfo.cs
+++ src/SharpTL/CommonAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Resources;
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharpTL")]
-[assembly: AssemblyCopyright("Copyright © 2013 Alexander Logger.")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 Alexander Logger.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.5.1")]
+[assembly: AssemblyVersion("0.6")]
