fc0bb86c6fd564b047953350ace204bfe221f2ad
Alexander Logger <intagger@live.ru>
2014-02-25 00:15:13 +0400

    Changed constructor number from dec to hex in an exception.

--- src/SharpTL/SharpTL.PCL/Serializers/TLMultiConstructorObjectSerializer.cs
+++ src/SharpTL/SharpTL.PCL/Serializers/TLMultiConstructorObjectSerializer.cs
@@ -55,7 +55,7 @@ namespace SharpTL.Serializers
             ITLSingleConstructorSerializer serializer;
             if (!_serializersConstructorNumberIndex.TryGetValue(constructorNumber, out serializer))
             {
-                throw new NotSupportedException(string.Format("Construction number '{0}' is not supported by this serializer.", constructorNumber));
+                throw new NotSupportedException(string.Format("Construction number 0x{0:X} is not supported by this serializer.", constructorNumber));
             }
 
             return serializer.Read(context, TLSerializationMode.Bare);
